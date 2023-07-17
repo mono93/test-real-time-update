32e61247e2defc3e5d7b91e82fe1a58ff26373e7
mono93 <monoj.saha1993@gmail.com>
2023-07-17 20:53:04 +0530

    backend setup complete

--- test-real-time-update-server/src/app.module.ts
+++ test-real-time-update-server/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MessageGateway } from './message.gateway';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, MessageGateway],
 })
 export class AppModule {}
---
+++ test-real-time-update-server/src/message.gateway.ts
@@ -0,0 +1,36 @@
+import { Logger } from '@nestjs/common';
+import {
+    OnGatewayConnection,
+    OnGatewayDisconnect,
+    OnGatewayInit,
+    SubscribeMessage,
+    WebSocketGateway,
+    WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({ cors: true })
+export class MessageGateway
+    implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+
+    private logger: Logger = new Logger('MessageGateway');
+
+    @WebSocketServer() wss: Server;
+
+    afterInit(server: Server) {
+        this.logger.log('Initialized');
+    }
+
+    handleDisconnect(client: Socket) {
+        this.logger.log(`Client Disconnected: ${client.id}`);
+    }
+
+    handleConnection(client: Socket, ...args: any[]) {
+        this.logger.log(`Client Connected: ${client.id}`);
+    }
+
+    @SubscribeMessage('sendMessage')
+    async handleSendMessage(client: Socket, payload: string): Promise<void> {
+        this.wss.emit('receiveMessage',`client -> ${client.id} message -> message`);
+    }
+}
